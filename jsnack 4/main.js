d5dce7e5488feeac9552d8c5ad896431a0a51e03
BernardoCosmi <bernardo.daddo90@gmail.com>
2024-01-12 17:15:15 +0100

    jsnack 4

--- jsnack 4/main.js
+++ jsnack 4/main.js
@@ -2,3 +2,12 @@
 snack 4
 Inserisci un numero, se è pari stampa il numero, 
 se è dispari stampa il numero successivo. */
+
+let N = prompt("Inserisci un numero e premi invio")
+
+for(let i=N; i<=N; i++)
+    if(i % 2 === 0){
+        console.log('Numero pari: ' + i)
+    }else{
+        console.log('Numero dispari, il numero successivo è: ' + (+i + 1))
+}
